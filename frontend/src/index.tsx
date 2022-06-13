971654d9f6bc74049f08a5a22f7c4c1b50a203e8
Aki Foudila <aki.foudila@nordhealth.com>
2022-06-13 20:56:44 +0300

    Add camera preview to join form

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,34 +1,49 @@
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { render } from 'solid-js/web'
-import { Form, Button } from 'solid-bootstrap'
+import { Container, Row, Col, Form, Button } from 'solid-bootstrap'
+
+import './f-meet.css'
 
 
 function JoinForm() {
+  const video: HTMLVideoElement = <video muted={true} autoplay={true} /> as HTMLVideoElement
+
+  navigator.mediaDevices.getUserMedia({'video': true, 'audio': true}).then(stream => {
+    video.srcObject = stream
+  })
+
   return <Form>
-    <Form.Group class="mb-3" controlId="formName">
-      <Form.Label>Name</Form.Label>
-      <Form.Control type="text" placeholder="Enter name" />
-    </Form.Group>
+    <Row>
+      <Col>{video}</Col>
+      <Col>
+        <Form.Group class="mb-3" controlId="formName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter name" />
+        </Form.Group>
 
-    <Form.Group class="mb-3" controlId="formRoom">
-      <Form.Label>Room</Form.Label>
-      <Form.Control type="text" placeholder="Enter room to join" />
-    </Form.Group>
+        <Form.Group class="mb-3" controlId="formRoom">
+          <Form.Label>Room</Form.Label>
+          <Form.Control type="text" placeholder="Enter room to join" />
+        </Form.Group>
 
-    <Button variant="primary" type="submit">Submit</Button>
+        <Button variant="primary" type="submit">Submit</Button>
+      </Col>
+    </Row>
   </Form>
 }
 
 
 function App() {
   return <>
-    <div class="container">
-      <h1 class="display-1">F-Meet</h1>
-      <p class="lead">A video call application.</p>
+    <Container>
+      <div class="text-center">
+        <h1 class="display-1">F-Meet</h1>
+        <p class="lead">A video call application.</p>
+      </div>
 
       <JoinForm/>
-    </div>
+    </Container>
   </>
 }
 
